e52b2abe78d4e1f37e0bfb7bb8aff60ed5b7a228
thakordixit567 <103193972+thakordixit567@users.noreply.github.com>
2025-10-08 13:11:55 +0530

    UPDATE!

---
+++ components/CompanionForm.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { z } from "zod";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+
+const formSchema = z.object({
+  name: z.string().min(1, { message: " Companion is required. " }),
+  subject: z.string().min(1, { message: " Subject is required. " }),
+  topic: z.string().min(1, { message: " Topic is required. " }),
+  voice: z.string().min(1, { message: " Voice is required. " }),
+  style: z.string().min(1, { message: "  Style is required. " }),
+  duration: z.coerce.number().min(1, { message: " Duration is required. " }),
+});
+
+const CompanionForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      subject: "",
+      topic: "",
+      voice: "",
+      style: "",
+      duration: 15,
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Companion name</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Enter the companion name"
+                  {...field}
+                  className="input"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default CompanionForm;
