54a4a9ff7a05a6bccc66c8e64e09ed0b3be9f42c
thakordixit567 <103193972+thakordixit567@users.noreply.github.com>
2025-11-01 18:30:29 +0530

    UPDATE!

--- components/Searchinput.tsx
+++ components/Searchinput.tsx
@@ -13,15 +13,15 @@ const Searchinput = () => {
 
   const [searchQuery, setsearchQuery] = useState("");
   return (
-    <div className=" relative border border-black rounded-lg items-center flex gap-2 px-2 py-1 h-fit ">
-      <Image src='/icons/search.svg' alt='search' width={15} height={15} />
-      <Input 
-      placeholder="Search companions..."
-      className="outline-none"
-      value={searchQuery}
-      onChange={(e) => setsearchQuery(e.target.value)}
-      />
-    </div>
+   <div className="relative border border-black rounded-lg items-center flex gap-2 px-2 py-1 h-fit">
+            <Image src="/icons/search.svg" alt="search" width={15} height={15} />
+            <input
+                placeholder="Search companions..."
+                className="outline-none"
+                value={searchQuery}
+                onChange={(e) => (e.target.value)}
+            />
+        </div>
   );
 };
 
