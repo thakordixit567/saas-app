c5ee6b56475b08c957f7ec32ba29004ebba201d5
thakordixit567 <103193972+thakordixit567@users.noreply.github.com>
2025-11-08 14:37:24 +0530

    UPDATE!

--- app/Companions/[id]/page.tsx
+++ app/Companions/[id]/page.tsx
@@ -12,13 +12,13 @@ interface CompanionSessionPageProps {
 
 const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
   const { id } = await params;
-  const  { name, subject, title, topic, duration } = await getCompanion(id);
+  const companion = await getCompanion(id);
   const user = await currentUser();
+
+  const { name, subject, title, topic, duration } = companion;
 
   if (!user) redirect("/sign-in");
   if (!name) redirect("/Companions");
-
-  
 
   return (
     <main>
@@ -39,20 +39,22 @@ const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
           <div className=" flex flex-col gap-2 ">
             <div className=" flex items-center gap-2 ">
               <p className=" font-bold text-2xl ">{name}</p>
-              <div className="subject-badge max-sm:hidden">
-                   {subject}
-              </div>
+              <div className="subject-badge max-sm:hidden">{subject}</div>
             </div>
-            <p className=" text-lg ">
-               {topic}
-            </p>
+            <p className=" text-lg ">{topic}</p>
           </div>
         </div>
         <div className=" items-start text-2xl max-md:hidden ">
           {duration} min
         </div>
       </article>
-      <CompanionComponents/>
+      <CompanionComponents
+       {...companion} 
+       companionId={id}
+       userName={user?.firstName}
+       userImage={user?.imageUrl}
+
+       />
     </main>
   );
 };
--- components/CompanionComponents.tsx
+++ components/CompanionComponents.tsx
@@ -1,9 +1,73 @@
-import React from 'react'
+"use client";
 
-const CompanionComponents = () => {
-  return (
-    <div>CompanionComponents</div>
-  )
+import { cn, getSubjectColor } from "@/lib/utils";
+import { vapi } from "@/lib/vapi.sdk";
+import { error } from "console";
+import { useEffect, useState } from "react";
+
+enum CallStatus {
+  INACTIVE = "INACTIVE",
+  CONNECTING = "CONNECTING",
+  ACTIVE = "ACTIVE",
+  FINISHED = " FINISHED",
 }
 
-export default CompanionComponents
+const CompanionComponents = ({
+  companionId,
+  subject,
+  topic,
+  name,
+  userName,
+  userImage,
+  style,
+  voice,
+}: CompanionComponentProps) => {
+  const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
+  const [isSpeaking, setIsSpeaking] = useState(false);
+  useEffect(() => {
+    const onCallStart = () => setCallStatus(CallStatus.ACTIVE);
+
+    const onCallEnd = () => setCallStatus(CallStatus.FINISHED);
+
+    const onMessage = {};
+
+    const onSpeechStart = () => setIsSpeaking(true);
+    const onSpeechEnd = () => setIsSpeaking(false);
+
+    const onError = (error: Error) => console.log("Error", error);
+
+    vapi.on("call-start", onCallStart);
+    vapi.on("call-end", onCallEnd);
+    vapi.on("message", onMessage);
+    vapi.on("error", onError);
+    vapi.on("speech-start", onSpeechStart);
+    vapi.on("speech-end", onSpeechEnd);
+
+    return () => {
+      vapi.off("call-start", onCallStart);
+    vapi.off("call-end", onCallEnd);
+    vapi.off("message", onMessage);
+    vapi.off("error", onError);
+    vapi.off("speech-start", onSpeechStart);
+    vapi.off("speech-end", onSpeechEnd);
+    }
+  }, []);
+  return (
+    <section className=" flex flex-col h-[70vh] ">
+      <section className=" flex gap-8 max-sm:flex-col ">
+        <div className=" companion-section ">
+          <div
+            className="companion-avatar"
+            style={{ backgroundColor: getSubjectColor(subject) }}
+          >
+            <div
+              className={cn(" absolute transition-opacity duration-1000 ")}
+            ></div>
+          </div>
+        </div>
+      </section>
+    </section>
+  );
+};
+
+export default CompanionComponents;
